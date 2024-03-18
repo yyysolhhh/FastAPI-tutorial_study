cb4ab2b999277f57e59971c71fae7da7a539fd03
yyysolhhh <solbaram37@gmail.com>
2024-03-18 17:07:38 +0900

    Rename: split main.py

--- T01_Path_Parameters/main.py
+++ T01_Path_Parameters/main.py
@@ -16,8 +16,3 @@ async def read_user(user_id: str):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
