301466d488828a7756f99b2c570430a03c35e13b
yyysolhhh <solbaram37@gmail.com>
2024-03-19 01:47:16 +0900

    [Query_Parameters] multiple path and query parameters

--- T02_Query_Parameters/main.py
+++ T02_Query_Parameters/main.py
@@ -7,6 +7,20 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
     item = {"item_id": item_id}
