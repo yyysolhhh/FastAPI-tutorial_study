015a09498f6af16eb75b2864de6058f48064cb8a
yyysolhhh <solbaram37@gmail.com>
2024-03-18 16:20:22 +0900

    [Path_Parameters] order matters

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
