3390c6be308fe7164ad80b7f57b954cbcd49c361
yyysolhhh <solbaram37@gmail.com>
2024-03-19 02:37:27 +0900

    [Query_Parameters] required query parameters

--- T02_Query_Parameters/main.py
+++ T02_Query_Parameters/main.py
@@ -7,6 +7,20 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
+@app.get("/items/{item_id}")
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+
+
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_user_item(
         user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
