ef79b16077d24c2e16ff3e91ce920b9eee6a8dff
yyysolhhh <solbaram37@gmail.com>
2024-03-18 23:59:20 +0900

    [Query_Parameters] optional parameters

--- T02_Query_Parameters/main.py
+++ T02_Query_Parameters/main.py
@@ -1,8 +1,17 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
 
 @app.get("/items/")
