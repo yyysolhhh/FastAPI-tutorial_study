574faab0baa8fd4ccea3cecda15f58181159353b
yyysolhhh <solbaram37@gmail.com>
2024-03-19 15:29:18 +0900

    [Request_Body] use the model

--- T03_Request_Body/main.py
+++ T03_Request_Body/main.py
@@ -14,4 +14,8 @@ app = FastAPI()
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
